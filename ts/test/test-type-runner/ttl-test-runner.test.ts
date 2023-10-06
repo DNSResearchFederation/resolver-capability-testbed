3709396fe145665b0175ed2ebe0e898f722be6be
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2023-10-06 10:29:54 +0100

    Update ttl-test-runner.test.ts

--- ts/test/test-type-runner/ttl-test-runner.test.ts
+++ ts/test/test-type-runner/ttl-test-runner.test.ts
@@ -2,7 +2,14 @@ import TtlTestRunner from "../../src/test-type-runner/ttl-test-runner";
 
 describe("Test single host query test runner tests", function () {
 
-    it("Should be able to request with a unique prefix", () => {
+    var originalTimeout;
+
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 11000;
+    });
+
+    it("Should be able to request with a unique prefix", async () => {
 
         let testRunner = new TtlTestRunner();
         testRunner.runTest("test.com", []);
@@ -12,24 +19,36 @@ describe("Test single host query test runner tests", function () {
         // First request to ttl10;
         expect(testRunner.getPreviousRequests()[0].hostname).toContain("ttl10.test.com");
         expect(testRunner.getPreviousRequests()[0].hostname).toContain("https://");
-        expect(testRunner.getPreviousRequests()[0].hostname.length).toEqual(59);
+        expect(testRunner.getPreviousRequests()[0].hostname).toContain("?request=1");
+        expect(testRunner.getPreviousRequests()[0].hostname.length).toEqual(69);
 
         // First request to ttl15
         expect(testRunner.getPreviousRequests()[1].hostname).toContain("ttl15.test.com");
         expect(testRunner.getPreviousRequests()[1].hostname).toContain("https://");
-        expect(testRunner.getPreviousRequests()[1].hostname.length).toEqual(59);
+        expect(testRunner.getPreviousRequests()[1].hostname).toContain("?request=1");
+        expect(testRunner.getPreviousRequests()[1].hostname.length).toEqual(69);
 
         // Wait 10s
-        testRunner.wait(10000);
+        await testRunner.wait(10000);
 
         expect(testRunner.getPreviousRequests().length).toEqual(4);
 
         // Second request to ttl10
-        expect(testRunner.getPreviousRequests()[2].hostname).toEqual(testRunner.getPreviousRequests()[0].hostname);
+        expect(testRunner.getPreviousRequests()[2].hostname).toContain("ttl10.test.com");
+        expect(testRunner.getPreviousRequests()[2].hostname).toContain("https://");
+        expect(testRunner.getPreviousRequests()[2].hostname).toContain("?request=2");
+        expect(testRunner.getPreviousRequests()[2].hostname.length).toEqual(69);
 
         // Second request to ttl15
-        expect(testRunner.getPreviousRequests()[3].hostname).toEqual(testRunner.getPreviousRequests()[2].hostname);
+        expect(testRunner.getPreviousRequests()[3].hostname).toContain("ttl15.test.com");
+        expect(testRunner.getPreviousRequests()[3].hostname).toContain("https://");
+        expect(testRunner.getPreviousRequests()[3].hostname).toContain("?request=2");
+        expect(testRunner.getPreviousRequests()[3].hostname.length).toEqual(69);
 
+    });
+
+    afterEach(function() {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
     });
 
 });
